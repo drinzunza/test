ebe2e0793cc3b8d0cc5241ed22fba6c79d670909
bahodurova <amirkhon.bahodurov@gmail.com>
2019-11-06 11:05:16 +0300

    fix db issues

--- src/main/java/com/uav_recon/app/api/entities/db/PasswordResetAttempt.kt
+++ src/main/java/com/uav_recon/app/api/entities/db/PasswordResetAttempt.kt
@@ -7,7 +7,7 @@ import javax.persistence.*
 @Table(name = "password_reset_attempts")
 class PasswordResetAttempt(
         @Id
-        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "password_reset_attempt_id_seq")
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "password_reset_attempts_id_seq")
         var id: Long? = null,
         @Column(name = "user_id")
         val userId: Long,
