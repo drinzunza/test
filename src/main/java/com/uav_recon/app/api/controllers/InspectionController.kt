5fae24a26f86dcab7b8375c30a1143e3cd56f440
Sergey Lohanov <sergey.lohanov@omega-r.com>
2019-11-19 15:32:32 +0300

    Fixed inspections by userId

--- src/main/java/com/uav_recon/app/api/controllers/InspectionController.kt
+++ src/main/java/com/uav_recon/app/api/controllers/InspectionController.kt
@@ -18,7 +18,7 @@ class InspectionController(private val inspectionService: InspectionService) : B
 
     @GetMapping
     fun get(): ResponseEntity<List<InspectionDto>> {
-        return ResponseEntity.ok(inspectionService.listNotDeleted())
+        return ResponseEntity.ok(inspectionService.listNotDeleted(getAuthenticatedUserId()))
     }
 
     @PostMapping
