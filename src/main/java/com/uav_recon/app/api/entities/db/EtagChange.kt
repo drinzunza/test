07adb84ef7e82ae03660473859e43ec51d0ab948
Sergey Lohanov <sergey.lohanov@omega-r.com>
2019-12-05 18:09:02 +0300

    Added etag comment

--- src/main/java/com/uav_recon/app/api/entities/db/EtagChange.kt
+++ src/main/java/com/uav_recon/app/api/entities/db/EtagChange.kt
@@ -1,7 +1,10 @@
 package com.uav_recon.app.api.entities.db
 
 import java.io.Serializable
-
+/*
+    TODO добавить генератор
+    При создании сущности (кроме структуры) обязательно должен приходить родительский элемент
+ */
 class EtagChange : Serializable {
     val defects: List<String> = mutableListOf()
     val conditions: List<String> = mutableListOf()
