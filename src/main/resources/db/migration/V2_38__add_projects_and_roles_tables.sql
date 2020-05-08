42b7fd1710a53f60705bf18e7d569c62a3e88993
Sergey Lohanov <sergey.lohanov@omega-r.com>
2020-05-08 12:58:43 +0300

    Added company owners

--- src/main/resources/db/migration/V2_38__add_projects_and_roles_tables.sql
+++ src/main/resources/db/migration/V2_38__add_projects_and_roles_tables.sql
@@ -1,3 +1,9 @@
+CREATE TABLE project_structures (
+	id	SERIAL PRIMARY KEY NOT NULL,
+	project_id INT NOT NULL,
+	structure_id INT NOT NULL
+);
+
 CREATE TABLE projects (
 	id	SERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(50) NOT NULL,
@@ -14,12 +20,6 @@ CREATE TRIGGER set_projects_updated_at
     ON projects
     FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_updated_at();
-
-CREATE TABLE project_structures (
-	id	SERIAL PRIMARY KEY NOT NULL,
-	project_id INT NOT NULL,
-	structure_id INT NOT NULL
-);
 
 CREATE TABLE roles (
 	id	SERIAL PRIMARY KEY NOT NULL,
@@ -39,10 +39,20 @@ CREATE TABLE inspection_users (
 	user_id INT NOT NULL
 );
 
+CREATE TABLE company_owners (
+	id	SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(255) NOT NULL
+);
+
 INSERT INTO roles (name) VALUES ('Admin'),('Project Manager'),('Inspector'),('Guest');
 
 ALTER TABLE observation_defects ADD COLUMN clock_position INT;
 ALTER TABLE observation_defects ADD COLUMN repair_date TIMESTAMP WITH TIME ZONE;
 ALTER TABLE observation_defects ADD COLUMN repair_method VARCHAR(255);
+
 ALTER TABLE inspections ADD COLUMN project_id INT;
 ALTER TABLE users ADD COLUMN admin BOOLEAN default false;
+
+ALTER TABLE structures ADD COLUMN company_owner_id INT;
+INSERT INTO company_owners (name) VALUES ('Los Angeles Metropoliten');
+UPDATE structures SET company_owner_id = 1 WHERE company_owner_id IS NULL;
