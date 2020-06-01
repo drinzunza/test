c0b552af7492db5bdca75159bde7d59dfc3a90bb
Sergey Lohanov <sergey.lohanov@omega-r.com>
2020-06-01 16:03:19 +0300

    Fixed filtering mobile structures

--- src/main/java/com/uav_recon/app/api/repositories/InspectionRepository.kt
+++ src/main/java/com/uav_recon/app/api/repositories/InspectionRepository.kt
@@ -12,6 +12,7 @@ interface InspectionRepository : JpaRepository<Inspection, String> {
     fun findAllByDeletedIsFalseAndCreatedBy(userId: Int) : List<Inspection>
     fun findAllByDeletedIsFalseAndCreatedByIn(ids: List<Int>) : List<Inspection>
     fun findAllByDeletedIsFalseAndUuidInAndCreatedByIn(ids: List<String>, userIds: List<Int>) : List<Inspection>
+    fun findAllByUuidInOrCreatedByIn(ids: List<String>, userIds: List<Int>) : List<Inspection>
     fun findAllByDeletedIsFalseAndUuidIn(ids: List<String>) : List<Inspection>
     fun findByUuidAndDeletedIsFalse(id: String) : Optional<Inspection>
     fun findFirstByUuidAndDeletedIsFalse(id: String) : Inspection?
