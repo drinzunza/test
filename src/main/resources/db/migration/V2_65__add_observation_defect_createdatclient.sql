2f540d17425bdc475a333ed7fdda43d2c4c03905
Sergey Lohanov <sergey.lohanov@omega-r.com>
2021-01-15 16:16:32 +0300

    Fixed migration

--- src/main/resources/db/migration/V2_65__add_observation_defect_createdatclient.sql
+++ src/main/resources/db/migration/V2_65__add_observation_defect_createdatclient.sql
@@ -5,4 +5,11 @@ UPDATE observation_defects as od SET created_at_client =
         SUBSTRING(id, LENGTH(id) - 7, 2), '-',
         SUBSTRING(id, LENGTH(id) - 5, 2))
         FROM observation_defects WHERE uuid = od.uuid)::timestamp
-WHERE SUBSTRING(id, LENGTH(id) - 8, 1) = '-';
+WHERE
+    SUBSTRING(id, LENGTH(id) - 8, 1) = '-' AND
+    SUBSTRING(id, LENGTH(id) - 3, 4) > '2015' AND
+    SUBSTRING(id, LENGTH(id) - 3, 4) < '2022' AND
+    SUBSTRING(id, LENGTH(id) - 7, 2) > '00' AND
+    SUBSTRING(id, LENGTH(id) - 7, 2) < '13' AND
+    SUBSTRING(id, LENGTH(id) - 5, 2) > '00' AND
+    SUBSTRING(id, LENGTH(id) - 5, 2) < '31';
