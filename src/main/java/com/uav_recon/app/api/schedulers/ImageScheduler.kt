c745525a7d0a198fe31ac556a0889e4c6e5d72e4
Sergey Lohanov <sergey.lohanov@omega-r.com>
2021-01-15 12:19:07 +0300

    Disabled not need now thumb schedulers

--- src/main/java/com/uav_recon/app/api/schedulers/ImageScheduler.kt
+++ src/main/java/com/uav_recon/app/api/schedulers/ImageScheduler.kt
@@ -23,18 +23,18 @@ class ImageScheduler(
 //        }
 //        logger.info("scheduler stopped: runGenerateThumbnails ({} ms)", Date().time - timestamp)
 //    }
-
-    @Scheduled(initialDelay = 10000, fixedRate = 86400000)
-    fun runRegenerateIncorrectThumbnails() {
-        val timestamp = Date().time
-        logger.info("scheduler started: runRegenerateIncorrectThumbnails")
-        photoRepository.findAll().forEach { photo ->
-            val rect = fileService.getRect(photo.drawables)
-            if (rect != null && (rect.startX > rect.endX || rect.startY > rect.endY)) {
-                logger.info("regenerate ${photo.uuid}")
-                fileService.regenerateRectImages(photo)
-            }
-        }
-        logger.info("scheduler stopped: runRegenerateIncorrectThumbnails ({} ms)", Date().time - timestamp)
-    }
+//
+//    @Scheduled(initialDelay = 10000, fixedRate = 86400000)
+//    fun runRegenerateIncorrectThumbnails() {
+//        val timestamp = Date().time
+//        logger.info("scheduler started: runRegenerateIncorrectThumbnails")
+//        photoRepository.findAll().forEach { photo ->
+//            val rect = fileService.getRect(photo.drawables)
+//            if (rect != null && (rect.startX > rect.endX || rect.startY > rect.endY)) {
+//                logger.info("regenerate ${photo.uuid}")
+//                fileService.regenerateRectImages(photo)
+//            }
+//        }
+//        logger.info("scheduler stopped: runRegenerateIncorrectThumbnails ({} ms)", Date().time - timestamp)
+//    }
 }
