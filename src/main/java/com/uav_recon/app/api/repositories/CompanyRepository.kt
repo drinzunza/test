7d4980ee99666431e9f472a64cae7dd3957eaf9f
sergeyzhuravlev <jyravlevsergey@gmail.com>
2022-03-05 15:31:34 +0300

    Added companies to get modify dictionaries

--- src/main/java/com/uav_recon/app/api/repositories/CompanyRepository.kt
+++ src/main/java/com/uav_recon/app/api/repositories/CompanyRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository
 interface CompanyRepository : CrudRepository<Company, Long> {
     fun findFirstById(companyId: Long): Company?
     fun findFirstByDeletedIsFalseAndId(id: Long) : Company?
+    fun findAllByDeletedIsFalseAndIdIn(ids: List<Long>): List<Company>
     fun findAllByDeletedIsFalseAndType(type: CompanyType) : List<Company>
     fun findAllByDeletedIsFalseAndTypeAndCreatorCompanyId(type: CompanyType, creatorCompanyId: Long) : List<Company>
     fun findAllByDeletedIsFalseAndCreatorCompanyId(creatorCompanyId: Long) : List<Company>
