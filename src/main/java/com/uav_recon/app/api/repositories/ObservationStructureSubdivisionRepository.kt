aff244a08ad55df9b03ab1977837382ce6c6d240
Kim Vinson <kim@ingenuity.ph>
2022-09-21 18:21:28 +0800

    Create repositories for StructureSubdivision and ObservationStructureSubdivision

---
+++ src/main/java/com/uav_recon/app/api/repositories/ObservationStructureSubdivisionRepository.kt
@@ -0,0 +1,10 @@
+package com.uav_recon.app.api.repositories
+
+import com.uav_recon.app.api.entities.db.ObservationStructureSubdivision
+import org.springframework.data.repository.CrudRepository
+
+interface ObservationStructureSubdivisionRepository : CrudRepository<ObservationStructureSubdivision, String> {
+    fun findAllByObservationId(observationId: String): List<ObservationStructureSubdivision>
+
+    fun findFirstByUuid(uuid: String): ObservationStructureSubdivision?
+}
