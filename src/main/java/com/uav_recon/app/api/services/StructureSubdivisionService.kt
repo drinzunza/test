0c91f4eb99a8149649233028d5532c8e8e962d6a
Kim Vinson <kim@ingenuity.ph>
2022-10-03 16:53:32 +0800

    Enforce subdivision number uniqueness within an inspection

--- src/main/java/com/uav_recon/app/api/services/StructureSubdivisionService.kt
+++ src/main/java/com/uav_recon/app/api/services/StructureSubdivisionService.kt
@@ -3,6 +3,7 @@ package com.uav_recon.app.api.services
 import com.uav_recon.app.api.entities.db.toDto
 import com.uav_recon.app.api.entities.requests.bridge.StructureSubdivisionDto
 import com.uav_recon.app.api.entities.requests.bridge.toEntity
+import com.uav_recon.app.api.repositories.InspectionRepository
 import com.uav_recon.app.api.repositories.StructureSubdivisionRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,12 +11,24 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class StructureSubdivisionService(
     private val structureSubdivisionRepository: StructureSubdivisionRepository,
-    private val observationStructureSubdivisionService: ObservationStructureSubdivisionService
+    private val observationStructureSubdivisionService: ObservationStructureSubdivisionService,
+    private val inspectionRepository: InspectionRepository
 ) : BaseService() {
 
     @Transactional
+    @Throws(Error::class)
     fun save(inspectionId: String, dto: StructureSubdivisionDto): StructureSubdivisionDto {
         dto.inspectionId = inspectionId
+
+        // check if inspection exists
+        inspectionRepository.findFirstByUuid(inspectionId)
+            ?: throw Error(400, "Inspection does not exist")
+
+        // check if subdivision number is unique for the inspection
+        val existingSubdivisions = structureSubdivisionRepository.findAllByInspectionId(inspectionId)
+        if (existingSubdivisions.any { it.number == dto.number }) {
+            throw Error(400, "Subdivision number already exists for this inspection")
+        }
 
         val saved = structureSubdivisionRepository.save(dto.toEntity())
 
