a15bf2628af98f9fcaf219ad8de18af9a30c9a14
Jeremy Patrick Pacabis <jeremy@ingenuity.ph>
2023-02-13 17:40:36 +0800

    feat: add new db table columns: observations.computed_health_index, observation_structure_subdivision.computed_health_index and structure_subdivision.computed_sgr_rating

---
+++ src/main/java/com/uav_recon/app/api/schedulers/ComputedValuesScheduler.kt
@@ -0,0 +1,10 @@
+package com.uav_recon.app.api.schedulers
+
+/**
+ * Created by jeremypacabis on Feb 13, 2023 @ 13:06.
+ * @author Jeremy Patrick Pacabis <jeremypacabis@gmail.com>
+ * @author Jeremy Patrick Pacabis <jeremy@ingenuity.ph>
+ * com.uav_recon.app.api.schedulers <uav-recon>
+ */
+class ComputedValuesScheduler {
+}
